a573cfe07958bab8e4ad8a5dc345731407127dbb
nnaidu <nnaidu@walmartlabs.com>
2015-07-02 18:33:02 -0700

    Integrate Logging and update pom.
    - Integrate SLF4J binding.
    - Include slf4j-log4j12 dependency in pom.
    - Add log4j.xml configuration file.
    - Add logging in ServiceImpl.
    - Move dependency version in pom to properties section.

--- src/main/java/com/experiment/poc/endpoint/ServiceImpl.java
+++ src/main/java/com/experiment/poc/endpoint/ServiceImpl.java
@@ -1,11 +1,17 @@
 package com.experiment.poc.endpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.experiment.poc.exception.BaseException;
 import com.experiment.poc.utils.CommonUtils;
 
 public class ServiceImpl implements Service {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ServiceImpl.class);
+
 	public String getWelcomeMessage(String name) throws BaseException {
+		LOG.debug("welcomeAPI with parameter: {}", name);
 		if (CommonUtils.isStringNullOrEmpty(name)) {
 			throw new BaseException("Empty or null name");
 		}
